36ba1c257a6b08e08865014c1332f3268113e95d
hosam faour <hosamf1992@gmail.com>
2019-10-23 11:46:18 +0300

    added gallery

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,1 +1,23 @@
 'use strict'
+
+
+
+
+function onInit(){
+    renderGallery();
+}
+
+
+
+function renderGallery() {
+    var strHtml = '';
+
+    gImgs.forEach((img) => {
+        strHtml += `<div class='items'> <img data-id="${img.id}"  src="${img.url}"> </div>`;
+
+
+    });
+
+    var elGallery = document.querySelector('.main-gallery');
+    elGallery.innerHTML = strHtml;
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,1 +1,33 @@
 'use strict'
+
+
+var gKeywords = { 'happy': 12, 'funny puk': 1 }
+
+var gImgs = [
+    { id: 1, url: 'img/gallery/1.jpg', keywords: ['happy'] },
+    { id: 2, url: 'img/gallery/2.jpg', keywords: ['happy'] },
+    { id: 3, url: 'img/gallery/3.jpg', keywords: ['happy'] },
+    { id: 4, url: 'img/gallery/4.jpg', keywords: ['happy'] },
+    { id: 5, url: 'img/gallery/5.jpg', keywords: ['happy'] },
+    { id: 6, url: 'img/gallery/6.jpg', keywords: ['happy'] },
+    { id: 7, url: 'img/gallery/7.jpg', keywords: ['happy'] },
+    { id: 8, url: 'img/gallery/8.jpg', keywords: ['happy'] },
+    { id: 9, url: 'img/gallery/9.jpg', keywords: ['happy'] },
+
+
+];
+
+var gMeme = {
+    selectedImgId: 5,
+    selectedTxtIdx: 0,
+
+    txts: [
+        {
+            line: 'I never eat Falafel',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }]
+};
+
+
